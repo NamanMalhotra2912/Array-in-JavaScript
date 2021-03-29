15539e352a240385f5a1f0746e474bcf78c4101a
NamanMalhotra2912 <neerajmalhotra0001@gmail.com>
2021-03-30 00:56:47 +0530

    Array Program

---
+++ Array.js
@@ -0,0 +1,9 @@
+var cars = ["Maruti", "Suzuki","BMW","Toyota"];
+
+var carsNew = new Array ("Baleno", "Swift","Kia","Fortuner", 50);
+var first = carsNew[0];
+var last = carsNew[carsNew.length-1];
+
+console.log(carsNew);
+console.log(first);
+console.log(last);

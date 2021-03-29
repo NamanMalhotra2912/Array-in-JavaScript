e9c3f36c16c40adc905a4c7ffeef56c5e8ce52ee
NamanMalhotra2912 <neerajmalhotra0001@gmail.com>
2021-03-30 01:00:36 +0530

    Genrate random numbers

---
+++ RandomNumber.js
@@ -0,0 +1,9 @@
+let minNumber = 100;
+let maxNumber = 999;
+let arrayNumber = [];
+// let number = Math.floor(Math.random() * (maxNumber-minNumber))+ minNumber;
+
+for (let i =1; i<=10; i++){
+    let number = Math.floor(Math.random() * (maxNumber-minNumber))+ minNumber;
+    console.log(number);
+}
